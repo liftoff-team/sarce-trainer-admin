3fa6c32f84f827af32b19d7290aaccf99a904d25
Fabien Albi <fabien.albi@icloud.com>
2016-05-18 16:04:04 +0200

    chore(factory): add user factory
    finish the user factory with factorygirl
    
    close issue #9

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,5 +3,10 @@ FactoryGirl.define do
   factory :user do
     first_name 'John'
     last_name 'Doe'
+    email 'john.doe@wanadoo.fr'
+    password 'test1234'
+    password_confirmation 'test1234'
+    cis 'grenoble'
+    rank_id 1
   end
 end
