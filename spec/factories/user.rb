f938e2aaa7eab0b8bf858bdacff6ea850d58436a
semia <semia.ladjal@hotmail.fr>
2016-05-19 18:36:35 +0300

    chore(factories): refactor User factory
    Use Faker and Sequence to randomize test user
    
    close issue #11

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,12 +1,12 @@
 FactoryGirl.define do
-
   factory :user do
-    first_name 'John'
-    last_name 'Doe'
-    email 'john.doe@wanadoo.fr'
-    password 'test1234'
-    password_confirmation 'test1234'
-    cis 'grenoble'
+    password = Faker::Internet.password
+    first_name Faker::Name.first_name
+    last_name Faker::Name.last_name
+    sequence(:email) { |n| "pompier#{n}@exemple.com" }
+    password password
+    password_confirmation password
+    cis Faker::Address.city
     rank 'sgt'
   end
 end
