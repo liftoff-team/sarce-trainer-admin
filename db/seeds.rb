10e36954c4eb0d3099b444a613c4b9f163896fe5
Arnaud Lenglet <arlenglet@gmail.com>
2016-05-23 00:01:53 +0200

    chore(seed): feed seed file with basic data (user creation) +
    > add comment to User factory to explain why we need hard coded password

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+def create_sarce_trainer_minimal_stack
+  create_users
+end
+
+def create_users
+  FactoryGirl.create_list(:user, 2)
+  puts "- 2 user created"
+end
+
+create_sarce_trainer_minimal_stack
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    password = "azerty"
+    # We don't generate the password with Faker, because we'll use this
+    # factory in the seed file, and we want to be able to log in with this user later. :)
+    password = "pomplar"
 
     first_name              Faker::Name.first_name
     last_name               Faker::Name.last_name
