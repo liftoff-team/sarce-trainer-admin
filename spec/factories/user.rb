d270d192705f60b290eb29cbbb3220ad73a35789
Arnaud Lenglet <arlenglet@gmail.com>
2016-05-22 23:32:19 +0200

    refactor(factory): refactory the user factory to use a hardcoded password value + format the file

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,12 +1,13 @@
 FactoryGirl.define do
   factory :user do
-    password = Faker::Internet.password
-    first_name Faker::Name.first_name
-    last_name Faker::Name.last_name
-    sequence(:email) { |n| "pompier#{n}@exemple.com" }
-    password password
-    password_confirmation password
-    cis Faker::Address.city
-    rank 'sgt'
+    password = "azerty"
+
+    first_name              Faker::Name.first_name
+    last_name               Faker::Name.last_name
+    sequence(:email)        { |n| "pompier#{n}@exemple.com" }
+    password                password
+    password_confirmation   password
+    cis                     Faker::Address.city
+    rank                    'sgt'
   end
 end
