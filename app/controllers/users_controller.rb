ca2c8eda1f87752b6e389fbe6f43be8a720b1a41
semia <semia.ladjal@hotmail.fr>
2016-05-25 12:14:00 +0300

    feat(views): Add user index for admin User index display all user data for admin and redirect to root if not admin
    close issue #19

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+
+    unless current_user.is_admin?
+      redirect_to root_path, notice: "You're not allowed to access
+                                    this area: get out!"
+    end
+  end
+
   def show
     @user = User.find(params[:id])
     @rank = Rank.const_get(:RANKS)[@user.rank.to_sym]
