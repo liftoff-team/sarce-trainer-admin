72a0cb4eee07a670b3d8ee79ebd848a5edf110e2
Fabien Albi <fabien.albi@icloud.com>
2016-05-26 10:41:51 +0200

    chore(db): add documentation seeds
    Close Issue#24

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'factory_girl'
+require 'Faker'
+# require all factories
+Dir[Rails.root.join('spec/factories/*.rb')].each { |f| require f }
+
 
 def create_sarce_trainer_minimal_stack
   create_users
+  create_documentations
 end
 
 def create_users
   FactoryGirl.create_list(:user, 2)
-  puts "- 2 user created"
+  puts "- 2 users created"
+end
+
+def create_documentations
+  FactoryGirl.create_list(:documentation, 5)
+  puts "- 5 documentations created"
 end
 
 create_sarce_trainer_minimal_stack
