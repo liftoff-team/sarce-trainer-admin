9f52903acc525456cf80b7b132c45c4a04dbee26
Fabien Albi <fabien.albi@icloud.com>
2016-06-20 10:45:23 +0200

    feat(Home): Add redirections to replace home page
    No user sign in --> redirect to sign in page
    Admin signed in --> redirect to questions#index page
    Non-admin signed in --> redirect to my_profile page
    After sign in --> redirect to correspondent 'home' page
    
    Issue #52/53/54/55

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
+    if current_user
+      if current_user.is_admin
+        redirect_to questions_path
+      else
+        redirect_to my_profile_path
+      end
+    else
+      redirect_to new_user_session_path
+    end
   end
 end
