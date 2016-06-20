d95f8acbe5d9c8cead2aea10dc2b11975008c745
Fabien Albi <fabien.albi@icloud.com>
2016-06-20 11:56:40 +0200

    refactor(specs): removes the need for test db seeding
    Issue #62

--- spec/support/configs/database_cleaner.rb
+++ spec/support/configs/database_cleaner.rb
@@ -1,9 +1,6 @@
 RSpec.configure do |config|
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
-    # Use of truncation strategy for models with uniqueness validations
-    DatabaseCleaner.strategy = :truncation, {:only => %w[users daily_questions]}
-    DatabaseCleaner.clean
   end
 
   config.before(:each) do
