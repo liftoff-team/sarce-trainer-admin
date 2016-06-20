73d534645aa14dc17e697c40a6761436965060e9
Fabien Albi <fabien.albi@icloud.com>
2016-06-20 18:32:02 +0200

    feat(decorators): add Draper gem and creates question decorator

--- app/services/question_service.rb
+++ app/services/question_service.rb
@@ -3,6 +3,7 @@ class QuestionService
     @question = Question.find(question_id)
   end
 
+  # TODO: (fabienalbi) maybe move this method into presenter
   def find_correct_count
     GivenAnswer.where(question_id: @question.id, is_correct: true).count
   end
---
+++ spec/decorators/question_decorator_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe QuestionDecorator do
+  describe '#last_answers' do
+      
+  end
+end
