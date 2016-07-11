b34b30f30fde1f5ca443f9dcd6ef32240bc131cd
semia <semia.ladjal@hotmail.fr>
2016-07-11 10:58:30 +0200

    feat(config): add configuration to send email +
    + in environnement developpement
    + in environnement production
    + change mailer application. Add default from and subject

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -49,6 +49,17 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  config.action_mailer.perform_deliveries = true
   # default_url_options used by devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.smtp_settings = {
+    user_name:      ENV['SENDMAIL_USERNAME'],
+    password:       ENV['SENDMAIL_PASSWORD'],
+    domain:         ENV['MAIL_HOST'],
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+}
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,5 +64,23 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   Rails.application.routes.default_url_options[:host] =
-    'sarce-trainer-admin.herokuapp.com'
+    'www.sarce-trainer-admin.herokuapp.com'
+
+config.action_mailer.default_url_options = { host: "www.sarce-trainer-admin.herokuapp.com" }
+
+ config.active_support.deprecation = :notify
+
+ config.action_mailer.delivery_method = :smtp
+ onfig.action_mailer.raise_delivery_errors =false
+ config.action_mailer.default :charset => "utf-8"
+
+ # SMTP settings
+ ActionMailer::Base.smtp_settings = {
+     :port           => 587,
+     :address        => 'smtp.gmail.com',
+     :domain         => 'gmail.com',
+     :user_name      => ENV['username'],
+     :password       => ENV['password'],
+     :authentication => :plain,
+ }
 end
