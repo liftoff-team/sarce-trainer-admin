8eef17c7a8d3a428d4af64472c72c6e7daecddf1
semia <semia.ladjal@hotmail.fr>
2016-07-12 17:16:29 +0200

    [wip]

--- app/controllers/my_profiles_controller.rb
+++ app/controllers/my_profiles_controller.rb
@@ -2,5 +2,9 @@ class MyProfilesController < ApplicationController
   def show
     @me = current_user
     @rank = Rank.const_get(:RANKS)[@me.rank.to_sym]
+    @questions = Question.all
+    @user = User.find_by_id(params[:id])
+    @given_answer = GivenAnswer.where(user_id: @user).limit(10)
   end
+
 end
