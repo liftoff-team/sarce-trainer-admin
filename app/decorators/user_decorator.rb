28fe57c877ba3dd236df94dd22c7cd852ee69a36
semia <semia.ladjal@hotmail.fr>
2016-07-16 16:03:52 +0200

    feat(decorators): add User#questions_counter +
    + add test #questions_counter
    + use in decorators/user_decorator
    
    close issue #129

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -4,4 +4,8 @@ class UserDecorator < Drape::Decorator
   def answered_counter
     GivenAnswer.where(user_id: model.id).count
   end
+
+  def questions_counter
+    Question.where(author_id: model.id).count
+  end
 end
